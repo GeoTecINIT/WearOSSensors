5d54c365450fd471dd0c20c300d80c7f5fadb3ef
Miguel Matey Sanz <al341802@uji.es>
2022-09-29 14:12:57 +0200

    Revert "refactor: change command client method visibility to ease extension"
    This reverts commit e1eef369a22858832bfb7993ee40f49f0e77e9da.

--- wearossensors/src/main/java/es/uji/geotec/wearossensors/command/CommandClient.java
+++ wearossensors/src/main/java/es/uji/geotec/wearossensors/command/CommandClient.java
@@ -38,7 +38,7 @@ public class CommandClient {
         sendCommand("stop-" + sensor.toString().toLowerCase());
     }
 
-    protected void sendCommand(String commandName, int sensorDelay, int batchSize) {
+    private void sendCommand(String commandName, int sensorDelay, int batchSize) {
         String command = commandFromParameters(commandName, sensorDelay, batchSize);
         sendCommand(command);
     }
